dbf272f57d7c84fb5ca730f0042bacaa97b4d5d0
MinjieWang <MinjieWang29@google.com>
2017-05-04 14:59:46 -0400

    second commit;

--- application/controllers/about.php
+++ application/controllers/about.php
@@ -1,7 +1,7 @@
 <?php
 class about extends CI_Controller {
     function index(){
-        $this->output->cache(1);
+        //$this->output->cache(1);
         $this->load->view('header.php');
         $this->load->view('about.php');
         $this->load->view('footer.php');
--- application/views/about.php
+++ application/views/about.php
@@ -1,76 +1,63 @@
-  <!--/banner-section-->
- <!--//main-header-->
-    <!-- breadcrumb -->
-	<div class="w3_breadcrumb">
-	<div class="breadcrumb-inner">	
-			<ul>
-				<li><a href="index.html">Home</a> <i> /</i></li>
-				<li>About</li>
-			</ul>
-		</div>
-	</div>
-<!-- //breadcrumb -->
-			<!--/content-inner-section-->
-				<div class="w3_content_agilleinfo_inner">
-					<div class="container">
-					      <div class="inner-agile-w3l-part-head">
-					            <h2 class="w3l-inner-h-title">About</h2>
-							</div>
-							<div class="ab-w3l-spa">
-							 <img src="images/ab.jpg" alt="" class="img-responsive" />
-								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. ever since the 1500s.Itaque earum rerum hic tenetur a sapiente delectus reiciendis maiores hasellusMaecenas ac hendrerit purus. Lorem ipsum dolor sit amet.Lorem Ipsum is simply dummy text of the printing and typesetting industry
-								</p>
-							</div>
-								<!-- services -->
-							<div class="w3_agileits_services_grids">
-								<div class="col-md-3 w3_agileits_services_grid hvr-overline-from-center">
-									<div class="w3_agileits_services_grid_agile">
-										<div class="w3_agileits_services_grid_1">
-											<img src="images/5.jpg" alt="service-img">
-										</div>
-										<h3>Deluxe Room</h3>
-										<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-									</div>
-								</div>
-								<div class="col-md-3 w3_agileits_services_grid hvr-overline-from-center">
-									<div class="w3_agileits_services_grid_agile">
-										<div class="w3_agileits_services_grid_1">
-											<img src="images/6.jpg" alt="service-img">
-										</div>
-										<h3>Luxury Room</h3>
-										<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-									</div>
-								</div>
-								<div class="col-md-3 w3_agileits_services_grid hvr-overline-from-center">
-									<div class="w3_agileits_services_grid_agile">
-										<div class="w3_agileits_services_grid_1">
-											<img src="images/7.jpg" alt="service-img">
-										</div>
-										<h3>Swimming Pool</h3>
-										<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-									</div>
-								</div>
-								<div class="col-md-3 w3_agileits_services_grid hvr-overline-from-center">
-									<div class="w3_agileits_services_grid_agile">
-										<div class="w3_agileits_services_grid_1">
-											<img src="images/8.jpg" alt="service-img">
-										</div>
-										<h3>Spa Care</h3>
-										<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-									</div>
-								</div>
-								<div class="clearfix"> </div>
-							</div>
+    <div class="w3_content_agilleinfo_inner">
+        <div class="container">
+            <div class="inner-agile-w3l-part-head">
+                <h2 class="w3l-inner-h-title">关于我们</h2>
+            </div>
+            <div class="ab-w3l-spa">
+                <img src="images/ab.jpg" alt="" class="img-responsive" />
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. ever since the 1500s.Itaque earum rerum hic tenetur a sapiente delectus reiciendis maiores hasellusMaecenas ac hendrerit purus. Lorem ipsum dolor sit amet.Lorem Ipsum is simply dummy text of the printing and typesetting industry
+                </p>
+            </div>
+            <!-- services -->
+            <div class="w3_agileits_services_grids">
+                <div class="col-md-3 w3_agileits_services_grid hvr-overline-from-center">
+                    <div class="w3_agileits_services_grid_agile">
+                        <div class="w3_agileits_services_grid_1">
+                                <img src="images/5.jpg" alt="service-img">
+                        </div>
+                        <h3>Deluxe Room</h3>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+                </div>
+                <div class="col-md-3 w3_agileits_services_grid hvr-overline-from-center">
+                    <div class="w3_agileits_services_grid_agile">
+                        <div class="w3_agileits_services_grid_1">
+                            <img src="images/6.jpg" alt="service-img">
+                        </div>
+                        <h3>Luxury Room</h3>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+                </div>
+                <div class="col-md-3 w3_agileits_services_grid hvr-overline-from-center">
+                    <div class="w3_agileits_services_grid_agile">
+                        <div class="w3_agileits_services_grid_1">
+                                <img src="images/7.jpg" alt="service-img">
+                        </div>
+                        <h3>Swimming Pool</h3>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+                </div>
+                <div class="col-md-3 w3_agileits_services_grid hvr-overline-from-center">
+                    <div class="w3_agileits_services_grid_agile">
+                        <div class="w3_agileits_services_grid_1">
+                            <img src="images/8.jpg" alt="service-img">
+                        </div>
+                        <h3>Spa Care</h3>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+                </div>
+                <div class="clearfix"> </div>
+            </div>
 				<!-- //services -->
 				
-				           <div class="agileinf-button">    <a class="read" href="single.html">
-											Read More
-							   </a>
-							   </div>
+            <div class="agileinf-button">    
+                <a class="read" href="single.html">Read More</a>
+            </div>
 
-					</div>
+        </div>
 
-		 	<div class="featured-facility">
+        <div class="featured-facility">
 	  <!-- welcome -->
 		
 			<div class="spa-agile">
@@ -382,162 +369,3 @@
 			
 			<!--//content-inner-section-->
 
- <!-- Footer -->
-	<div class="w3l-footer">
-		<div class="container">
-         <div class="footer-info-agile">
-				<div class="col-md-2 footer-info-grid links">
-					<h4>Quick links</h4>
-					<ul>
-						       <li><a href="index.html">Home</a></li> 
-								<li><a href="about.html">About</a></li> 
-								<li><a href="codes.html">Services</a></li> 
-								<li><a href="gallery.html">Gallery</a></li> 
-								<li><a href="contact.html">Contact</a></li> 
-					</ul>
-				</div>
-				<div class="col-md-3 footer-info-grid address">
-					<h4>Address</h4>
-					<address>
-						<ul>
-							<li>Mexico United States 3000</li>
-							<li>40019 Honey Street</li>
-							<li>BO,Mexico</li>
-							<li>Telephone : +1 (734) 123-4567</li>
-							<li>Email : <a class="mail" href="mailto:mail@example.com">info(at)example.com</a></li>
-						</ul>
-					</address>
-				</div>
-				<div class="col-md-3 footer-grid">
-				   <h4>Instagram</h4>
-					<div class="footer-grid-instagram">
-					<a href="#"><img src="images/f1.jpg" alt=" " class="img-responsive"></a>
-					</div>
-					<div class="footer-grid-instagram">
-					<a href="#"><img src="images/f2.jpg" alt=" " class="img-responsive"></a>
-					</div>
-					<div class="footer-grid-instagram">
-						<a href="#"><img src="images/f3.jpg" alt=" " class="img-responsive"></a>
-					</div>
-					<div class="footer-grid-instagram">
-					<a href="#"><img src="images/f4.jpg" alt=" " class="img-responsive"></a>
-					</div>
-					<div class="clearfix"> </div>
-				</div>
-				<div class="col-md-4 footer-info-grid">
-				<div class="connect-social">
-					<h4>Connect with us</h4>
-					<section class="social">
-                        <ul>
-							<li><a class="icon fb" href="#"><i class="fa fa-facebook"></i></a></li>
-							<li><a class="icon tw" href="#"><i class="fa fa-twitter"></i></a></li>
-							
-							
-							<li><a class="icon pin" href="#"><i class="fa fa-pinterest"></i></a></li>
-							<li><a class="icon db" href="#"><i class="fa fa-dribbble"></i></a></li>
-							<li><a class="icon gp" href="#"><i class="fa fa-google-plus"></i></a></li>
-						</ul>
-					</section>
-
-				</div>
-					
-
-					
-				</div>
-				<div class="clearfix"></div>
-			</div>
-
-			<div class="connect-agileits newsletter">
-				<h4>Newsletter</h4>
-					<p>Subscribe to our newsletter and we will inform you about newest projects and promotions.
-					</p>
-					<form action="#" method="post" class="newsletter">
-						<input class="email" type="email" placeholder="Your email address..." required="">
-						<input type="submit" class="submit" value="Subscribe">
-					</form>
-			</div>
-	   </div>
-     </div>
-
-			<!--w3agile_footer_copy -->
-			<div class="w3agile_footer_copy">
-				    <p>Copyright &copy; 2017.Company name All rights reserved.<a target="_blank" href="http://sc.chinaz.com/moban/">&#x7F51;&#x9875;&#x6A21;&#x677F;</a></p>
-			</div>
-		<a href="#home" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
-	<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
-	<!-- Dropdown-Menu-JavaScript -->
-			<script>
-				$(document).ready(function(){
-					$(".dropdown").hover(            
-						function() {
-							$('.dropdown-menu', this).stop( true, true ).slideDown("fast");
-							$(this).toggleClass('open');        
-						},
-						function() {
-							$('.dropdown-menu', this).stop( true, true ).slideUp("fast");
-							$(this).toggleClass('open');       
-						}
-					);
-				});
-			</script>
-		<!-- //Dropdown-Menu-JavaScript -->
-
-
-<script type="text/javascript" src="js/jquery.zoomslider.min.js"></script>
-		<!-- search-jQuery -->
-				<script src="js/main.js"></script>
-
-<!--/script-->
-			<!-- flexSlider -->
-					<script defer src="js/jquery.flexslider.js"></script>
-					<script type="text/javascript">
-					$(window).load(function(){
-					  $('.flexslider').flexslider({
-						animation: "slide",
-						start: function(slider){
-						  $('body').removeClass('loading');
-						}
-					  });
-					});
-				  </script>
-	<!-- Calendar -->
-				<link rel="stylesheet" href="css/jquery-ui.css" />
-				<script src="js/jquery-ui.js"></script>
-				  <script>
-						  $(function() {
-							$( "#datepicker,#datepicker1,#datepicker2,#datepicker3" ).datepicker();
-						  });
-				  </script>
-			<!-- //Calendar -->
-<script type="text/javascript" src="js/move-top.js"></script>
-<script type="text/javascript" src="js/easing.js"></script>
-
-<script type="text/javascript">
-			jQuery(document).ready(function($) {
-				$(".scroll").click(function(event){		
-					event.preventDefault();
-					$('html,body').animate({scrollTop:$(this.hash).offset().top},900);
-				});
-			});
-</script>
- <script type="text/javascript">
-						$(document).ready(function() {
-							/*
-							var defaults = {
-					  			containerID: 'toTop', // fading element id
-								containerHoverID: 'toTopHover', // fading element hover id
-								scrollSpeed: 1200,
-								easingType: 'linear' 
-					 		};
-							*/
-							
-							$().UItoTop({ easingType: 'easeOutQuart' });
-							
-						});
-					</script>
-<!--end-smooth-scrolling-->
-<!--js for bootstrap working-->
-	<script src="js/bootstrap.js"></script>
-<!-- //for bootstrap working -->
-</body>
-</html>
